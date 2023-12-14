82178d85799f74e99c817f57e677edd45a8bf7ce
signoutz <aunkaffpom@gmail.com>
2023-12-14 16:10:18 +0700

    upload

---
+++ info.php
@@ -0,0 +1,21 @@
+<?php
+    $url='https://hpc3.cmuccdc.org/';
+    $description = 'เว็บไซต์วัดค่าฝุ่น PM2.5 โดย ศูนย์ข้อมูลการเปลี่ยนแปลงสภาพภูมิอากาศ มหาวิทยาลัยเชียงใหม่';
+    $title = 'ศูนย์อนามัยที่ 3 นครสวรรค์ by CCDC';
+    $icon='logo_hpc3.png';
+    $icon_title='logo_hpc3.ico';
+    $contact_title = 'ศูนย์อนามัยที่ 3 นครสวรรค์';
+    $contact_detail = 'ศูนย์อนามัยที่ 3 นครสวรรค์ เลขที่ 157 ม.1 ถ.พหลโยธิน ต.นครสวรรค์ออก อ.เมือง จ.นครสวรรค์ 60000
+    <br>เวลาทำการ วันจันทร์ - วันศุกร์ เวลา 8:30 - 16:30
+    <br>ปิดทำการ เสาร์-อาทิตย์ และวันหยุดนักขัตฤกษ์
+    <br>โทรศัพท์ 0-5625-5451-4
+    <br>โทรสาร 0-5625-5403;
+    <br>อีเมล hpc3@anamai.mail.go.th';
+    $facebook = 'https://www.facebook.com/HPC3Channel/';
+    // $youtube = 'https://www.youtube.com/channel/UCwlSb-f1YEwYG9s0o7LWRnA';
+    // $twitter = 'https://twitter.com/cmuofficial_tw?s=09';
+    // $instagram = 'https://www.instagram.com/cmuofficial.ig/';
+    $web = 'https://hpc3.anamai.moph.go.th/';
+    $footer = 'Copyright &copy; 2020. All rights reserved by CCDC.';
+    // $footer_repoting = 'Copyright &copy; 2020. All rights reserved by CCDC.';
+?>
